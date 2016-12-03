7f4a86bdcea3374d80c6c1779415ba54b8ef9ae6
Luan Sangguk Lee <sangguk258@gmail.com>
2016-12-04 01:22:37 +0900

    Revert "Initialized with webpack, babel, eslint and ReactJS"

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './components/App'
-
-require('babel-polyfill')
-
-ReactDom.render(<App />, window.document.getElementById('main'))
